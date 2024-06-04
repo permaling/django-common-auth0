2d438cc0c09ae059ebead2134cc24bf6c80a1a86
David Baum <serjantik@gmail.com>
2024-06-04 16:57:31 +0200

    Added real bersion number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 if __name__ == "__main__":
     setuptools.setup(
-        version=os.environ.get("PACKAGE_VERSION", "0.0.0"),
+        version=os.environ.get("PACKAGE_VERSION", "1.0.0"),
         install_requires=[
             'pilkit @ git+https://github.com/jokerinteractive/pilkit.git@e2eb73e1798865a201e570fced0bac195b2a590c',
             'admin_multiupload @ git+https://github.com/python-force/django-admin-multiupload.git@master#egg=admin_multiupload-1.10',
